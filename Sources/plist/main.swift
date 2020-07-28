5f5a76b605dd1f2ff2b634815b80292c813b5758
zmeriksen <zmeriksen@icloud.com>
2020-07-28 17:52:19 -0500

    Cleaned up code and added ability to create plist file

--- Sources/plist/main.swift
+++ Sources/plist/main.swift
@@ -4,11 +4,14 @@ import ArgumentParser
 enum PListError: Error {
     case noFile
     case noPList
+    case noKey
 }
 
+struct MockPList: Codable {}
+
 struct PList: ParsableCommand {
-    @Argument()
-    var file: String
+    @Option()
+    var new = false
     
     @Option()
     var output = false
@@ -17,21 +20,39 @@ struct PList: ParsableCommand {
     var addValues = false
     
     @Argument()
-    var key: String
+    var filePath: String
+    
+    @Argument()
+    var key: String?
     
     @Argument()
     var value: String?
     
-    mutating func run() throws {
-        guard let filePath = FileManager.default.contents(atPath: file) else {
+    mutating func createNewFile() throws {
+        if !filePath.hasPrefix(".plist") {
+            filePath.append(".plist")
+        }
+        let contents = try PropertyListEncoder().encode(MockPList())
+        // Create and add key-value
+        FileManager.default.createFile(atPath: filePath, contents: contents, attributes: [:])
+        
+        guard let file = FileManager.default.contents(atPath: filePath) else {
             throw PListError.noFile
         }
         
-        guard let plist = try? PropertyListSerialization.propertyList(from: filePath,
+        try editPList(file: file)
+    }
+    
+    func editPList(file: Data) throws {
+        guard let plist = try? PropertyListSerialization.propertyList(from: file,
                                                                       options: .mutableContainersAndLeaves,
                                                                       format: nil),
             var dictionary = plist as? [String: Any] else {
                 throw PListError.noPList
+        }
+        
+        guard let key = key else {
+            throw PListError.noKey
         }
         
         if addValues {
@@ -43,6 +64,18 @@ struct PList: ParsableCommand {
         if output {
             print(dictionary)
         }
+    }
+    
+    mutating func run() throws {
+        guard !new else {
+            return try createNewFile()
+        }
+        
+        guard let file = FileManager.default.contents(atPath: filePath) else {
+            throw PListError.noFile
+        }
+        
+        try editPList(file: file)
     }
 }
 
